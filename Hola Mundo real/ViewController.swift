5b9f22dab8ec13511ecc35459460006da362aefd
joaquin ruiz <joaco@MacBook-Pro-de-joaquin.local>
2017-03-04 23:41:18 -0300

    comentario

--- Hola Mundo real/ViewController.swift
+++ Hola Mundo real/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var massageLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeMassage(sender: AnyObject) {
+        massageLabel.text = "hola \(nameTextField.text)"
+        view.endEditing(true)
+    }
+    //comentary
 }
 
